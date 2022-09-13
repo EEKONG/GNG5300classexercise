9bcf3cd53602c9b3ad7a562362761c2cff598ca2
EEKONG <100251825+EEKONG@users.noreply.github.com>
2022-09-13 19:49:59 -0400

    Add files via upload

---
+++ class_exercise.py
@@ -0,0 +1,10 @@
+#add two numbers
+
+def addnumber(num1, num2):
+    return num1 + num2
+
+first = float(input("Enter first number :"));
+second = float(input("Enter second number :"));
+
+print("The sum of your numbers is", addnumber(first, second))
+
